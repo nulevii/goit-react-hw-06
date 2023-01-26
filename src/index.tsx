e16396f0e30cd05f65a2e73153f63a18c93031ce
Cherniugov Yevgenii <cherniugov@gmail.com>
2023-01-26 14:43:00 +0200

    Add Persistor to save contacts in Local Storage

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { store } from './store/store'
+import { PersistGate } from 'redux-persist/lib/integration/react'
+
+import { persistor, store } from './store/store'
 import './index.css'
 import App from './App'
 import 'normalize.css'
@@ -10,9 +12,11 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 )
 root.render(
-    <React.StrictMode>
-        <Provider store={store}>
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
         <App />
-        </Provider>
-    </React.StrictMode>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 )
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
 import contactsReducer from './features/contacts/contactsSlice'
 import filterReducer from './features/filter/filterSlice'
 
+const persistConfig = {
+  key: 'root',
+  storage
+}
+
+const contactsPersistReducer = persistReducer(persistConfig, contactsReducer)
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: contactsPersistReducer,
     filter: filterReducer
   }
 })
+
+export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
